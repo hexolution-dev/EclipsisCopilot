71662c16be74647d279918eb5cb970ffccf69d54
Hexolution <108171915+hexolution-dev@users.noreply.github.com>
2022-12-08 18:16:56 -0400

    Update ScreenReader.h

--- ScreenReader.h
+++ ScreenReader.h
@@ -1,45 +1,46 @@
 #include <iostream>
 #include <Windows.h>
+#include <vector>
 
+#include "MakePPMImage.h";
+
+// Class to read the pixels of the screen and create a PPM image
 class ScreenReader
 {
 public:
-
-    // define a function to read the screen pixels
+    // Reads the pixels of the screen and creates a PPM image
     void ReadScreenPixels(HWND hwnd, int screenWidth, int screenHeight)
     {
-        // create a bitmap to store the screen data
+        // Get the device context of the screen
         HDC hdcScreen = GetDC(hwnd);
+        // Create a compatible device context
         HDC hdcMemDC = CreateCompatibleDC(hdcScreen);
 
+        // Create a compatible bitmap for the screen
         HBITMAP hScreen = CreateCompatibleBitmap(hdcScreen, screenWidth, screenHeight);
+        // Select the bitmap into the compatible device context
         HGDIOBJ hOldBitmap = SelectObject(hdcMemDC, hScreen);
 
-        // copy the screen data into the bitmap
+        // Copy the screen into the compatible device context
         BitBlt(hdcMemDC, 0, 0, screenWidth, screenHeight, hdcScreen, 0, 0, SRCCOPY);
 
-        // create an array to store the RGB values
+        // Get the bitmap data in bytes
         int widthInBytes = screenWidth * 4;
         char* data = new char[widthInBytes * screenHeight];
 
-        // copy the RGB values into the array
-        GetBitmapBits(hScreen, widthInBytes * screenHeight, data);
+        // Copy the bitmap data into the buffer
+        GetBitmapBits(hScreen, screenWidth * 4 * screenHeight, data);
 
-        // write the RGB values to the file
-        for (int i = 0; i < screenHeight; i++)
-        {
-            for (int j = 0; j < screenWidth; j++)
-            {
-                int red = data[i * widthInBytes + j * 4];
-                int green = data[i * widthInBytes + j * 4 + 1];
-                int blue = data[i * widthInBytes + j * 4 + 2];
-            }
-        }
+        // Create a PPM image from the bitmap data (commented out for now)
+        // MakePPMImage ppm;
+        // ppm.createPPMImage(data, screenWidth, screenHeight, widthInBytes);
 
+        // Clean up
         SelectObject(hdcMemDC, hOldBitmap);
         DeleteDC(hdcMemDC);
         ReleaseDC(hwnd, hdcScreen);
         DeleteObject(hScreen);
+
         delete[] data;
     }
 };
